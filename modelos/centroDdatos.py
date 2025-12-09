6b09dd6a70f34931b7879923c301a8218d26e533
Xiloj01 <3752751180101@ingenieria.edu.gt>
2025-12-09 00:46:30 -0600

    202406139 y 202400822 agregan archivo xml y le dan una estructura al proyecto
    Co-authored-by: alisondepaz <alisondepaz@users.noreply.github.com>

---
+++ modelos/centroDdatos.py
@@ -0,0 +1,6 @@
+
+# from estructuras.lista_simple import ListaSimpleEnlazada
+
+class CentroDeDatos:
+    def __init__(self, id_centro, nombre, pais, ciudad, cpu_total, ram_total, almacenamiento_total):
+        pass
